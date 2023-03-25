9f313e478450b082b14698a18f3d4e173aff6328
Dmitriy <snipere@list.ru>
2023-03-25 16:10:11 +1000

    Added beer collection cell

---
+++ Beer/Views/BeerCollectionViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  BeerCollectionViewCell.swift
+//  Beer
+//
+//  Created by Дмитрий Лубов on 25.03.2023.
+//
+
+import UIKit
+
+final class BeerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageBeer: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    private let networkManager = NetworkManager.shared
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        activityIndicator.hidesWhenStopped = true
+    }
+    
+    func configure(with beer: Beer) {
+    }
+}
+
+// MARK: - Private Methods
+private extension BeerCollectionViewCell {
+    func imageWillApear() {
+        imageBeer.isHidden = true
+        activityIndicator.startAnimating()
+    }
+    
+    func imageDidApear() {
+        imageBeer.isHidden = false
+        activityIndicator.stopAnimating()
+    }
+    
+    func fetchImage(from url: URL) {
+        networkManager.fetchImage(from: url) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.imageBeer.image = UIImage(data: imageData)
+            case .failure(_):
+                self?.imageBeer.image = UIImage(systemName: "camera")
+                self?.imageBeer.tintColor = .white
+            }
+            self?.imageDidApear()
+        }
+    }
+}

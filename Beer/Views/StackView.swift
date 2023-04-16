62eeb484488294687647adc221b93900776056c6
Dmitriy <snipere@list.ru>
2023-04-16 12:43:32 +1000

    Added stack view factory

---
+++ Beer/Views/StackView.swift
@@ -0,0 +1,32 @@
+//
+//  StackView.swift
+//  Beer
+//
+//  Created by Дмитрий Лубов on 13.04.2023.
+//
+
+import UIKit
+
+protocol StackViewFactory {
+    func createStackView() -> UIStackView
+}
+
+class VerticalStackViewFactory: StackViewFactory {
+    let subViews: [UIView]
+    let spacing: CGFloat
+    
+    init(subViews: UIView..., spacing: CGFloat) {
+        self.subViews = subViews
+        self.spacing = spacing
+    }
+    
+    func createStackView() -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: subViews)
+        
+        stackView.axis = .vertical
+        stackView.spacing = spacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }
+}
